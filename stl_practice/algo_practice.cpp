dfffb6a3ad19067b10ff97d568828209becb85e8
liliangnike <liliangnike@126.com>
2026-08-18 06:37:48 +0000

    Add STL vector and MyVector testing code

---
+++ stl_practice/algo_practice.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    std::cout << "=== STL Algorithm Practice === \n"; 
+    std::cout << "\n=== All algorithm practice cases passed!\n";
+    return 0;
+}
---
+++ tests/test_vector.cpp
@@ -0,0 +1,9 @@
+#include "my_vector.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "=== MyVector Tests ===\n";
+    std::cout << "\nAll tests passed!\n";
+    return 0;
+}
