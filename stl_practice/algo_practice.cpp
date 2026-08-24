a76d1839213729c42641082fb48c00c165066276
liliangnike <liliangnike@126.com>
2026-08-24 09:51:50 +0000

    Add standard STL vector practice

--- stl_practice/algo_practice.cpp
+++ stl_practice/algo_practice.cpp
@@ -1,9 +1,27 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <cassert>
+
+#define TEST(name) std::cout << "[ALGO] " << name << " ... "
+#define PASS()     std::cout << "PASS\n";
+void practice_sort()
+{
+    TEST("sort / stable_sort");
+
+    std::vector<int> v = {5, 1, 3, 4, 2};
+    std::sort(v.begin(), v.end());
+    assert(v == std::vector<int>({1, 2, 3, 4, 5}));
+
+    PASS();
+}
 
 int main()
 {
     std::cout << "=== STL Algorithm Practice === \n"; 
+
+    practice_sort();
+
     std::cout << "\n=== All algorithm practice cases passed!\n";
     return 0;
 }
