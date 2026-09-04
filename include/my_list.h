bf29ab34b8e707d69a2301f115065c37fa5f1452
liliangnike <liliangnike@126.com>
2026-09-04 08:27:15 +0000

    Add insert function, begin iterator and end iterator

--- include/my_list.h
+++ include/my_list.h
@@ -98,7 +98,10 @@ public:
         bool operator!=(const ConstIterator& other) const { return node != other.node; }
     };
 
-    MyList() : sentinel_(new Node()), size_(0) {}
+    MyList() : sentinel_(new Node()), size_(0) {
+        sentinel_->prev = sentinel_;
+        sentinel_->next = sentinel_;
+    }
     ~MyList() {
         delete sentinel_;
     }
@@ -106,7 +109,61 @@ public:
     using iterator = Iterator;
     using const_iterator = ConstIterator;
 
+    // insert new element and return the iterator of the new element
+    // Before inserting: ... ←→ prev_node ←→ pos_node ←→ ...
+    // After inserting : ... ←→ prev_node ←→ new_node ←→ pos_node ←→ ...
+    // returns iterator of new_node
+    iterator insert(iterator pos, const T& value) {
+        Node* new_node = new Node(value);
+        Node* pos_node = pos.node;
+        Node* prev_node = pos_node->prev;
+       
+        new_node->prev = prev_node;
+        new_node->next = pos_node;
+        prev_node->next = new_node;
+        pos_node->prev = new_node;
+
+        ++size_;
+
+        return Iterator(new_node);
+    }
+
+    iterator insert(iterator pos, T&& value) {
+        Node* new_node = new Node(std::move(value));
+        Node* pos_node = pos.node;
+        Node* prev_node = pos_node->prev;
+       
+        new_node->prev = prev_node;
+        new_node->next = pos_node;
+        prev_node->next = new_node;
+        pos_node->prev = new_node;
+
+        ++size_;
+
+        return Iterator(new_node);
+    }
+
+    void swap(MyList& other) noexcept {
+        std::swap(sentinel_, other.sentinel_);
+        std::swap(size_, other.size_);
+    }
+
+    // iterator
+    iterator begin() { return Iterator(sentinel_->next); }
+    iterator end()   { return Iterator(sentinel_); }
+
+    const_iterator begin() const { return ConstIterator(sentinel_->next); }
+    const_iterator end() const   { return ConstIterator(sentinel_); }
+
+    std::size_t size() const { return size_; }
+    bool empty() const       { return size_ == 0; }
 private:
+    // Define an effective data node without data saved. It will be used as head or tail node.
+    // Eliminate boundary check.
+    // sentinel_ ←→ node1 ←→ node2 ←→ ... ←→ sentinel_
+    // (head)                                   (tail)
+    // begin() is sentinel_->next
+    // end() is sentinel_
     Node* sentinel_;
     std::size_t size_;
 };
